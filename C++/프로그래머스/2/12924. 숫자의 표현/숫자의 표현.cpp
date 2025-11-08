9f6038452e0216a130b950d61f68f63b87bbdf2d
jaeseung9 <120431749+jaeseung9@users.noreply.github.com>
2025-11-08 21:22:22 +0900

    [level 2] Title: 숫자의 표현, Time: 0.04 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+    
+    for(int i=1; i<= n; i++)
+    {
+        int num =0;
+        for(int j=i; j<=n; j++)
+        {
+            num +=j;
+            if (num == n)
+            {
+                answer++;
+                break;
+            } else if (num > n)
+            {
+                break; 
+            }
+        }
+    }
+    
+    return answer;
+}
