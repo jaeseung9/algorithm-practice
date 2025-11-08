cb84d2d3a63c6c93accbf114d8954e1faf6874cc
jaeseung9 <120431749+jaeseung9@users.noreply.github.com>
2025-11-08 21:18:32 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp)
+{
+    int answer = 0;
+    
+    while(hp>0)
+    {
+        if(hp>=5) hp-=5;
+        else if(hp>=3)hp-=3;
+        else hp-=1;
+        
+        answer++;
+    }
+    return answer;
+}
