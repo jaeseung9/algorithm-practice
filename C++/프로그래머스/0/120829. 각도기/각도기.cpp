ea86b0a1f8f1ebc63260f96d2132ff5ec46c0eef
jaeseung9 <120431749+jaeseung9@users.noreply.github.com>
2025-11-08 21:15:10 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) 
+{
+    int answer = 1;
+    
+    if(angle==90) answer=2;
+    else if(angle>90 && angle<180)answer=3;
+    else if(angle==180) answer=4;
+
+    
+    return answer;
+}
