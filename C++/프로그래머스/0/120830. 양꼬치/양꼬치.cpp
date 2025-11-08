1b512d58c89fa77cdbafb196d12be8abdb9de841
jaeseung9 <120431749+jaeseung9@users.noreply.github.com>
2025-11-08 21:15:17 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120830. 양꼬치/양꼬치.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int k)
+{
+    int answer = 0;
+    
+    if(n > 9) answer = (n/10) * -2000;
+    answer += (n * 12000) + (k * 2000);
+    
+    return answer;
+}
