08c12830b22ecbc79bc214c95bc1b7f4f12cd31c
jaeseung9 <120431749+jaeseung9@users.noreply.github.com>
2025-11-18 18:54:56 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.28 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    
+    int num = x * n;
+    int sum = 0;
+    
+    for(int i=0; i < n; i++){
+        sum += x;
+        answer.push_back(sum);
+    }
+    
+    return answer;
+}
